82838e7ce086ba9aa46cf4e7377ff5364c88eae3
Barbar Ahmad <barbarahmad@hotmail.de>
2019-01-06 09:55:24 +0100

    Updates

---
+++ precache-manifest.b0a13d953ed06cd7253a486fbe2740e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "4e906868020524a177ae",
+    "url": "/todolist/static/js/main.4e906868.chunk.js"
+  },
+  {
+    "revision": "7cf7aa2fe713fe584dff",
+    "url": "/todolist/static/js/1.7cf7aa2f.chunk.js"
+  },
+  {
+    "revision": "4e906868020524a177ae",
+    "url": "/todolist/static/css/main.85d3e6a0.chunk.css"
+  },
+  {
+    "revision": "469d539dd1370baaa9cd8ad33e313b78",
+    "url": "/todolist/index.html"
+  }
+];
